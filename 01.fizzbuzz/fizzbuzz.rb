c77a247b3e7787784a5d21d1c23a0da887ffbcd8
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-04 08:41:15 +0900

    apply rubocop-fjord

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
-for num in 1..20
-    if num % 15 == 0
-        puts "FizzBuzz"
-    elsif num % 5 == 0
-        puts "Buzz"
-    elsif num % 3 ==0
-        puts "Fizz"
-    else
-        puts num
-    end 
+(1..20).each do |num|
+  if num % 15 == 0
+    puts 'FizzBuzz'
+  elsif num % 5 == 0
+    puts 'Buzz'
+  elsif num % 3 == 0
+    puts 'Fizz'
+  else
+    puts num
+  end
 end
