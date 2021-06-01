d8e91c7fabf9edaef4697cdd832609ceb4e6ada9
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-06-01 21:32:21 +0900

    コマンドラインからの引数分割
    配列に格納

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,5 +1,22 @@
-Dir.foreach(Dir.pwd) do |item|
-    next if item == '.' or item == '..' # カレントディレクトリと親ディレクトリを除外
-    next if item[0] == '.'　# 隠しファイルを除外
-    puts item
-  end
+require 'optparse'
+
+opt = OptionParser.new
+
+option = []
+
+# コマンドラインからのaの引数取得
+opt.on('-a') do |val|
+  option << 'a' unless val.nil?
+end
+
+# コマンドラインからのlの引数取得
+opt.on('-l') do |val|
+  option << 'l' unless val.nil?
+end
+
+# コマンドラインからのrの引数取得
+opt.on('-r') do |val|
+  option << 'r' unless val.nil?
+end
+
+opt.parse(ARGV)
