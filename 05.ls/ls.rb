fac63e00ed711292052bb4acfef37347425bb0ae
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-06-15 22:45:42 +0900

    オプション変数をグローバル変数へ変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,21 +4,21 @@ require 'etc'
 ### input 入力
 opt = OptionParser.new
 
-option = []
+$option = []
 
 # コマンドラインからのaの引数取得
 opt.on('-a') do |val|
-  option << 'a' unless val.nil?
+  $option << 'a' unless val.nil?
 end
 
 # コマンドラインからのlの引数取得
 opt.on('-l') do |val|
-  option << 'l' unless val.nil?
+  $option << 'l' unless val.nil?
 end
 
 # コマンドラインからのrの引数取得
 opt.on('-r') do |val|
-  option << 'r' unless val.nil?
+  $option << 'r' unless val.nil?
 end
 
 opt.parse(ARGV)
