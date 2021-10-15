99856cbdc701d50f88453e4edb22bbb842fe324f
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-10-16 08:34:39 +0900

    sum_scoreメソッドを呼び出すことで処理をシンプルに変更した

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -1,11 +1,14 @@
 require_relative './frame'
 
 class Game
+
+  attr_reader :points
+
   def initialize(score)
     scores = split_score(score)
     frames = split_frame(scores)
-    points = calc_score(frames)
-    p points
+    @points = calc_score(frames)
+    p @points
   end
 
   def split_score(score)
@@ -62,7 +65,7 @@ class Game
                  frames[idx].sum_score
                elsif frames[idx].strike? # ストライク
                  if frames[idx + 1].strike? # 次もストライクか確認
-                   10 + frames[idx + 1].first_shot.score + frames[idx + 2].first_shot.score
+                   10 + frames[idx + 1].sum_score + frames[idx + 2].first_shot.score
                  else
                    10 + frames[idx + 1].first_shot.score + frames[idx + 1].second_shot.score
                  end
