cd4f326664064230f95bb25480db1aaf3a1c0c17
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-11-04 15:16:36 +0900

    rubocop適用

--- 08.ls_object/lib/file_info.rb
+++ 08.ls_object/lib/file_info.rb
@@ -2,30 +2,29 @@ class FileInfo
   attr_reader :file
 
   def initialize(file_name)
-      @file = File::Stat.new(file_name)
+    @file = File::Stat.new(file_name)
   end
-  
+
   # ファイルのパーミッションを取得する関数
   def switch_file_mode(num)
     permission = { '7' => 'rwx', '6' => 'rw-', '5' => 'r-w', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x' }
     permission[num]
   end
 
-  def show_file_details()
+  def show_file_details
     detailes = file.directory? ? 'd' : '-'
 
     [-3, -2, -1].each do |i|
       detailes += switch_file_mode(file.mode.to_s(8).slice(i, 1))
     end
 
-    detailes += ' ' + file.nlink.to_s + ' '
+    detailes += " #{file.nlink} "
 
-    detailes += Etc.getpwuid(file.uid).name + ' '
-    detailes += Etc.getgrgid(file.gid).name + ' '
-    detailes += file.size.to_s + ' '
-    detailes += file.mtime.to_s + ' '
+    detailes += "#{Etc.getpwuid(file.uid).name} "
+    detailes += "#{Etc.getgrgid(file.gid).name} "
+    detailes += "#{file.size} "
+    detailes += "#{file.mtime} "
 
     detailes
   end
-
 end
--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -1,39 +1,37 @@
 require_relative '../lib/file_info'
 require_relative '../lib/display'
 
-
 class LS
-    def initialize(option)
-
-        # 隠しファイル判定
-        if  ! option.include?('a')
-            file_names = Dir.children(Dir.pwd)
-            file_names.reject! { |file_name| file_name[0] == "."} 
-        else
-            file_names = Dir.entries(Dir.pwd)
-        end
-
-        # 昇順と降順を判定
-        if ! option.include?('r')
-            file_names.sort!
-        else
-            file_names.reverse!
-        end
-
-        # 詳細情報出力の判定
-        if option.include?('l')
-            file_names.map! do |file_name|
-                file_info = FileInfo.new(file_name)
-                file_info.show_file_details + file_name
-            end
-        end
-
-        # 画面出力判定
-        display = Display.new()
-        if option.include?('l')
-            display.horizontal(file_names)
-        else
-            display.vertical(file_names)
-        end
+  def initialize(option)
+    # 隠しファイル判定
+    if !option.include?('a')
+      file_names = Dir.children(Dir.pwd)
+      file_names.reject! { |file_name| file_name[0] == '.' }
+    else
+      file_names = Dir.entries(Dir.pwd)
     end
+
+    # 昇順と降順を判定
+    if !option.include?('r')
+      file_names.sort!
+    else
+      file_names.reverse!
+    end
+
+    # 詳細情報出力の判定
+    if option.include?('l')
+      file_names.map! do |file_name|
+        file_info = FileInfo.new(file_name)
+        file_info.show_file_details + file_name
+      end
+    end
+
+    # 画面出力判定
+    display = Display.new
+    if option.include?('l')
+      display.horizontal(file_names)
+    else
+      display.vertical(file_names)
+    end
+  end
 end
