e90f2852f3144d3d9c6e5debe7e0965b1b412b78
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-11-04 15:37:14 +0900

    プライベートメソッドに変更

--- 08.ls_object/lib/file_info.rb
+++ 08.ls_object/lib/file_info.rb
@@ -5,14 +5,8 @@ class FileInfo
     @file = File::Stat.new(file_name)
   end
 
-  # ファイルのパーミッションを取得する関数
-  def switch_file_mode(num)
-    permission = { '7' => 'rwx', '6' => 'rw-', '5' => 'r-w', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x' }
-    permission[num]
-  end
-
   def show_file_details
-    detailes = file.directory? ? 'd' : '-'
+    detailes = is_directory?
 
     [-3, -2, -1].each do |i|
       detailes += switch_file_mode(file.mode.to_s(8).slice(i, 1))
@@ -26,5 +20,17 @@ class FileInfo
     detailes += "#{file.mtime} "
 
     detailes
+  end
+
+  private
+
+  def is_directory?
+    file.directory? ? 'd' : '-'
+  end
+
+  # ファイルのパーミッションを取得する関数
+  def switch_file_mode(num)
+    permission = { '7' => 'rwx', '6' => 'rw-', '5' => 'r-w', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x' }
+    permission[num]
   end
 end
