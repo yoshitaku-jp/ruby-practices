0f33451133a0b7c09fb3ff579f85b2024377a86c
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-11-23 08:36:19 +0900

    オプションlでのif判定を1回でやるように修正

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -18,16 +18,13 @@ class LS
     end
 
     # 詳細情報出力の判定
+    # 画面出力判定
+    display = Display.new
     if option.include?('l')
       file_names.map! do |file_name|
         file_info = FileInfo.new(file_name)
         file_info.show_file_details + file_name
       end
-    end
-
-    # 画面出力判定
-    display = Display.new
-    if option.include?('l')
       display.horizontal(file_names)
     else
       display.vertical(file_names)
