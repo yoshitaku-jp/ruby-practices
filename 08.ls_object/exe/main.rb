f4509db273d3ceae4df8a58fe545fad24f458e8e
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-11-23 08:25:50 +0900

    グローバル変数になっていたのを変更

--- 08.ls_object/exe/main.rb
+++ 08.ls_object/exe/main.rb
@@ -5,23 +5,23 @@ require 'etc'
 
 opt = OptionParser.new
 
-$option = []
+option = []
 
 # コマンドラインからのaの引数取得
 opt.on('-a') do |_val|
-  $option << 'a'
+  option << 'a'
 end
 
 # コマンドラインからのlの引数取得
 opt.on('-l') do |_val|
-  $option << 'l'
+  option << 'l'
 end
 
 # コマンドラインからのrの引数取得
 opt.on('-r') do |_val|
-  $option << 'r'
+  option << 'r'
 end
 
 opt.parse(ARGV)
 
-LS.new($option)
+LS.new(option)
